40257104e239765770ba365a0d722ea05bf4f1ef
Julio Cesar Vasquez <juliocvz.jcv@gmail.com>
2026-06-27 21:12:00 -0400

    Fix webhook verification

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ app.get("/", (req, res) => {
   });
 });
 app.get('/webhook', (req, res) => {
-  const VERIFY_TOKEN = "global_gs_2026_bot";
+  const VERIFY_TOKEN = process.env.WHATSAPP_VERIFY_TOKEN;
   const mode = req.query['hub.mode'];
   const token = req.query['hub.verify_token'];
   const challenge = req.query['hub.challenge'];
